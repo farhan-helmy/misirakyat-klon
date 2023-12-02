64de6dee23efb25496c0b2fe5f6aa5fd0b3ab78a
farhan-helmy <farhanhlmy@gmail.com>
2023-12-02 13:53:41 +0800

    feat: update order page

--- app/routes/order+/index.tsx
+++ app/routes/order+/index.tsx
@@ -15,47 +15,95 @@ import {
 } from '#app/components/ui/dialog.tsx'
 import { Input } from '#app/components/ui/input.tsx'
 import { Label } from '#app/components/ui/label.tsx'
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '#app/components/ui/select.tsx'
+
+const foodCategories = [
+	'Mie',
+	'Nasi',
+	'Sushi',
+	'Pizza',
+	'Burger',
+	'Salad',
+	'Pasta',
+	'DimSum',
+	'Steak',
+	'Ramen',
+	'Tacos',
+	'Eskem',
+	'Kebab',
+	'Soup',
+	'Curry',
+]
+
+function FoodTypeList() {
+	return (
+		<div className="flex flex-row gap-2 overflow-auto">
+			{foodCategories.map((category, index) => (
+				<div key={index}>
+					<div className="flex flex-col items-center justify-center rounded-md w-12">
+						
+                        <img src="/img/user.png" alt="user" className='h-8 w-8' />
+						<div>
+                            <p className='text-xs'>{category}</p>
+                        </div>
+					</div>
+				</div>
+			))}
+		</div>
+	)
+}
+
+function PilihKawasanDialog() {
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button>Pilih Kawasan</Button>
+			</DialogTrigger>
+			<DialogContent className="sm:max-w-md">
+				<DialogHeader>
+					<DialogTitle>Pilih kawasan</DialogTitle>
+					<DialogDescription>Pilih kawasan berdekatan anda.</DialogDescription>
+				</DialogHeader>
+				<div className="flex items-center space-x-2">
+					<div className="grid flex-1 gap-2">
+						<Label htmlFor="link" className="sr-only">
+							Negeri
+						</Label>
+						<Select>
+							<SelectTrigger>
+								<SelectValue placeholder="Negeri" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectItem value="light">Light</SelectItem>
+								<SelectItem value="dark">Dark</SelectItem>
+								<SelectItem value="system">System</SelectItem>
+							</SelectContent>
+						</Select>
+					</div>
+				</div>
+				<DialogFooter className="sm:justify-start">
+					<DialogClose asChild>
+						<Button type="button" variant="secondary">
+							Hantar
+						</Button>
+					</DialogClose>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	)
+}
 
 function Header() {
 	return (
 		<div className="flex flex-row justify-between">
 			<div>Misi Klon | Makanan</div>
-			<Dialog>
-				<DialogTrigger asChild>
-					<Button>Pilih Kawasan</Button>
-				</DialogTrigger>
-				<DialogContent className="sm:max-w-md">
-					<DialogHeader>
-						<DialogTitle>Share link</DialogTitle>
-						<DialogDescription>
-							Anyone who has this link will be able to view this.
-						</DialogDescription>
-					</DialogHeader>
-					<div className="flex items-center space-x-2">
-						<div className="grid flex-1 gap-2">
-							<Label htmlFor="link" className="sr-only">
-								Link
-							</Label>
-							<Input
-								id="link"
-								defaultValue="https://ui.shadcn.com/docs/installation"
-								readOnly
-							/>
-						</div>
-						<Button type="submit" size="sm" className="px-3">
-							<span className="sr-only">Copy</span>
-							<CopyIcon className="h-4 w-4" />
-						</Button>
-					</div>
-					<DialogFooter className="sm:justify-start">
-						<DialogClose asChild>
-							<Button type="button" variant="secondary">
-								Close
-							</Button>
-						</DialogClose>
-					</DialogFooter>
-				</DialogContent>
-			</Dialog>
+			<PilihKawasanDialog />
 		</div>
 	)
 }
@@ -70,6 +118,9 @@ export default function OrderRoute() {
 	return (
 		<div className="p-4">
 			<Header />
+			<div className="pt-8">
+				<FoodTypeList />
+			</div>
 			<h1>Order page</h1>
 		</div>
 	)
